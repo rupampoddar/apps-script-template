b1ac4f7dbff3e13bd701da97cc90d60d20237fb8
Rupam Poddar <rupam.poddar@protonmail.com>
2023-10-03 19:54:48 +0530

    feat(frontend): update logos

--- apps/gas-frontend/src/App.vue
+++ apps/gas-frontend/src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
+      <img src="https://vitejs.dev/logo-with-shadow.png" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="https://play.vuejs.org/logo.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -16,7 +16,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 .logo {
-  height: 6em;
+  height: 4em;
+  width: 4em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
